bcdf9a5873025be3d71e0d272fddce1965f5b7eb
swarnkarneeraj <sneeraj.itech@gmail.com>
2020-02-07 19:25:49 +0530

    node application with basic server and api

---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
+// sync code
+// const textIn = fs.readFileSync("./txt/newDoc.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is a new text and wanted in a new file with : ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/newDocFromCode.txt", textOut);
+// console.log("File has been wrriten");
+
+//Async Code
+
+// fs.readFile("./txt/newDoc.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+// console.log("File is read succussfully");
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  //Overview
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+    //Product
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  } // API
+  else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json"
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html"
+    });
+    res.end("<h1>The page could not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server is running on port: 8000");
+});
