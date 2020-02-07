127d6b39d64f174a73ba258aa6c87df447bccffb
swarnkarneeraj <sneeraj.itech@gmail.com>
2020-02-08 00:51:48 +0530

    added slugify pluging

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
-
-// sync code
-// const textIn = fs.readFileSync("./txt/newDoc.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is a new text and wanted in a new file with : ${textIn}.\n Created on ${Date.now()}`;
-// fs.writeFileSync("./txt/newDocFromCode.txt", textOut);
-// console.log("File has been wrriten");
-
-//Async Code
-
-// fs.readFile("./txt/newDoc.txt", "utf-8", (err, data) => {
-//   console.log(data);
-// });
-// console.log("File is read succussfully");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -34,6 +22,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
@@ -70,5 +61,19 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Server is running on port: 8000");
+  console.log("Server is listening on port: 8000");
 });
+
+// sync code
+// const textIn = fs.readFileSync("./txt/newDoc.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is a new text and wanted in a new file with : ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/newDocFromCode.txt", textOut);
+// console.log("File has been wrriten");
+
+//Async Code
+
+// fs.readFile("./txt/newDoc.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+// console.log("File is read succussfully");
